9a8884d5c03d277e3e35b45550a5237885f739db
bianshun <bsxiaoyao316@qq.com>
2021-05-13 15:22:39 +0800

    提交静态导入远程模块 Demo

---
+++ app1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slides from './Slides';
+const RemoteNewsList = React.lazy(() => import('app2/NewsList'));
+
+const App = () => (
+  <div>
+    <h2 style={{ textAlign: 'center' }}>App1, Local Slides, Remote NewsList</h2>
+    <Slides />
+    <React.Suspense fallback='Loading Slides'>
+      <RemoteNewsList />
+    </React.Suspense>
+  </div>
+);
+
+export default App;
---
+++ app1/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  devtool: false,
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    port: 3001,
+    contentBase: path.join(__dirname, "dist"),
+  },
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  //http://localhost:3002/remoteEntry.js
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Slides": "./src/Slides",
+      },
+      remotes: {
+        app2: "app2@http://localhost:3002/remoteEntry.js",
+      },
+      shared: {
+        react: { singleton: true },
+        "react-dom": { singleton: true }
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
